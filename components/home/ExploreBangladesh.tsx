35cbd17d69b5f9d958aa4b4865761c3ee2693d4e
hayatul-islam <hayatulemon340@gmail.com>
2026-03-28 23:39:25 +0600

    slider overflow issue fix

--- components/home/ExploreBangladesh.tsx
+++ components/home/ExploreBangladesh.tsx
@@ -15,8 +15,9 @@ const ExploreBangladesh: React.FC = () => {
     slidesToScroll: 4,
     arrows: false,
     responsive: [
-      { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 3 } },
-      { breakpoint: 640, settings: { slidesToShow: 1.5, slidesToScroll: 1.5 } },
+      { breakpoint: 1280, settings: { slidesToShow: 3, slidesToScroll: 3 } },
+      { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 2 } },
+      { breakpoint: 640, settings: { slidesToShow: 1, slidesToScroll: 1 } },
     ],
   };
 
@@ -29,7 +30,7 @@ const ExploreBangladesh: React.FC = () => {
           className="max-w-2xl text-center lg:text-left"
         />
 
-        <div className="explore-slider -mx-2">
+        <div className="explore-slider ">
           <Slider {...settings}>
             {DESTINATIONS.map((dest) => (
               <DestinationCard key={dest.id} {...dest} />
